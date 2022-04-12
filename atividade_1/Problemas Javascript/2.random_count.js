df07e59d3c5fdc448be56e0e19754f0778aa6519
itamar <itamardprf@gmail.com>
2022-04-12 20:42:11 -0300

    Typo fix

--- atividade_1/Problemas Javascript/2.random_count.js
+++ atividade_1/Problemas Javascript/2.random_count.js
@@ -3,12 +3,12 @@
 
 // A solucão vai gerar 10 números inteiros entre 0 a 9.
 random_array = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
-var integer_elements = 0
+var even_elements = 0
 
 for (var i = 0; i < random_array.length; i++){
   if (random_array[i] % 2 == 0){
-    integer_elements += 1;
+    even_elements += 1;
   }
 }
 console.log(random_array)
-console.log("Número de inteiros é igual a: ", integer_elements)
+console.log("Número de inteiros pares é igual a: ", even_elements)
