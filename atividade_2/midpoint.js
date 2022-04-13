305cde21bfa65f331a4c3546c1c77adb00bbe14e
Matheus Melo <matheus.correia@lavid.ufpb.br>
2022-04-12 21:13:52 -0300

    generalized-midpoint

--- atividade_2/midpoint.js
+++ atividade_2/midpoint.js
@@ -3,6 +3,95 @@
 let color_buffer = new Canvas("canvas");
 color_buffer.clear();
 
+function reflect_to_first(x1, y1, x2, y2) {
+
+  let dx = Math.abs(x2 - x1); // delta_x
+  let dy = Math.abs(y2 - y1); // delta_y
+
+
+  if(dx > dy) { // 1, 4, 5, 8
+    if ( x1 < x2  && y1 <= y2) { // 1
+      return {
+        a: { x: x1, y: y1 },
+        b: { x: x2, y: y2 },
+        octant: 1
+      };
+    }
+    if ( x2 < x1  && y1 <= y2) { // 4
+      return {
+        a: { x: -x1, y: y1 },
+        b: { x: -x2, y: y2 },
+        octant: 4
+      };
+    }
+    if ( x2 < x1  && y2 <= y1) { // 5
+      return {
+        a: { x: -x1, y: -y1 },
+        b: { x: -x2, y: -y2 },
+        octant: 5
+      };
+    }
+    if ( x1 < x2  && y2 <= y1) { // 8
+      return {
+        a: { x: x1, y: -y1 },
+        b: { x: x2, y: -y2 },
+        octant: 8
+      };
+    }
+  } else if (dy > dx) { // 2, 3, 6 e 7
+    if ( y1 < y2  && x1 <= x2) { // 2
+      return {
+        a: { x: y1, y: x1 },
+        b: { x: y2, y: x2 },
+        octant: 2
+      };
+    }
+    if ( y1 < y2  && x2 <= x1) { // 3
+      return {
+        a: { x: y1, y: -x1 },
+        b: { x: y2, y: -x2 },
+        octant: 3
+      };
+    }
+    if ( y2 < y1  && x2 <= x1) { // 6
+      return {
+        a: { x: -y1, y: x1 },
+        b: { x: -y2, y: x2 },
+        octant: 6
+      };
+    }
+    if ( y2 < y1  && x1 <= x2) { // 7
+      return {
+        a: { x: -y1, y: -x1 },
+        b: { x: -y2, y: -x2 },
+        octant: 7
+      };
+    }
+  }
+}
+
+function reflect_out_first(x, y, octant = 1) {
+
+  switch (octant) {
+    case 2:
+      return { x: y, y: x };
+    case 3:
+      return { x: -y, y: x };
+    case 4:
+      return { x: -x, y: y };
+    case 5:
+      return { x: -x, y: -y };
+    case 6:
+      return { x: y, y: -x };
+    case 7:
+      return { x: -y, y: -x };
+    case 8:
+      return { x: x, y: -y };
+    default:
+      return { x, y };    
+  }
+}
+
 function MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1) {
 	let dx = x1 - x0; // delta_x
   let dy = y1 - y0; // delta_y
@@ -13,13 +102,17 @@ function MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1) {
   let d = 2*dy-dx; // decision var for first pixel
   let incE = 2*dy;
   let incNE = 2*(dy-dx); 
-  let x = x0;
-  let y = y1;
 
-  color_buffer.putPixel(x, y, InterpolateColor(range, step, color_0, color_1));
+  let projection = reflect_to_first(x0, y0, x1, y1);
+  console.log(JSON.stringify(projection, null, 4));
+  let x = projection.a.x;
+  let y = projection.a.y;
+
+  color_buffer.putPixel(x0, y0, InterpolateColor(range, step, color_0, color_1)); // first point, no projection needed
   step++; // increment step to change next color;
 
-  while (x < x1) {
+  let projection_undone = {};
+  while (x < projection.b.x) {
     if (d <= 0) {
       d += incE;
       x++;
@@ -29,8 +122,10 @@ function MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1) {
       y++;
     }
 
-  color_buffer.putPixel(x, y, InterpolateColor(range, step, color_0, color_1));
-  step++; // increment step to change next color;
+    projection_undone = reflect_out_first(x, y, projection.octant);
+    // console.log(JSON.stringify(projection_undone, null, 4));
+    color_buffer.putPixel(projection_undone.x, projection_undone.y, InterpolateColor(range, step, color_0, color_1));
+    step++; // increment step to change next color;
   }
 }
 
@@ -40,7 +135,7 @@ function DrawTriangle(x0, y0, x1, y1, x2, y2, color_0, color_1, color_2) {
   // second/right edge
   MidPointLineAlgorithm(y1, x1, y2,x2, color_1, color_2);
   // third/bottom edge
-  MidPointLineAlgorithm(x2, y2, x0,y0, color_1, color_2); 
+  MidPointLineAlgorithm(x0, y0, x2,y2, color_1, color_2); 
 }
 
 function InterpolateColor(range, step, first_color, second_color) {
