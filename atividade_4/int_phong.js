52a9d832c8ca8c3f14dd1e57a53a992177c13233
Matheus Melo <matheus.correia@lavid.ufpb.br>
2022-06-12 22:52:21 -0300

    finalized-phong

--- atividade_4/int_phong.js
+++ atividade_4/int_phong.js
@@ -28,22 +28,20 @@ let geometry = new THREE.TorusGeometry(10, 3, 16, 25);
 //----------------------------------------------------------------------------
 // Variáveis do tipo "uniform", enviadas pela CPU aos shaders :
 //
-// * 'Ia' : intensidade (i.e cor) da luz ambiente.
+// * 'lightColor' : intensidade (i.e cor) da luz ambiente.
 // * 'lightPosition' : posição da fonte de luz pontual no Espaço do Universo.
-// * 'Ip_diffuse_color' : intensidade (i.e cor) do componente difuso da fonte de luz pontual.
+// * 'diffuseColor' : intensidade (i.e cor) do componente difuso da fonte de luz pontual.
 // * 'k_a' : coeficiente de reflectância ambiente do objeto.
 // * 'k_d' : coeficiente de reflectância difusa do objeto.
 // * 'k_s' : coeficiente de reflectância especular do objeto.
 //----------------------------------------------------------------------------
 let rendering_uniforms = {
-    Ia: {type: 'vec3', value: new THREE.Color(0.3, 0.3, 0.3)},
+    lightColor: {type: 'vec3', value: new THREE.Color(0.3, 0.3, 0.3)},
     lightPosition: {type: 'vec3', value: new THREE.Vector3(-20, 10, 10)},
-    Ip_diffuse_color: {type: 'vec3', value: new THREE.Color(0.7, 0.7, 0.7)},
+    diffuseColor: {type: 'vec3', value: new THREE.Color(0.7, 0.7, 0.7)},
     k_a: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
     k_d: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
     k_s: {type: 'vec3', value: new THREE.Color(1, 1, 1)},
-    lightColor: {type: 'vec3', value: new THREE.Color(1, 1, 1)},
-    objectColor: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
     viewPosition: {type: 'vec3', value: camera.position},
 }
 
@@ -83,23 +81,18 @@ material.vertexShader = `
 // Fragment Shader
 //----------------------------------------------------------------------------
 material.fragmentShader = `
-    // 'I' : valor de cor originalmente calculada pelo Vertex Shader, e já interpolada para o fragmento corrente.
-    
-    varying vec4 I;
   
     varying vec3 normalVector;  // Surface normal
     varying vec3 fragPosition;       // Vertex position
     
-    uniform float k_a;   // Ambient reflection coefficient
-    uniform float k_d;   // Diffuse reflection coefficient
-    uniform float k_s;   // Specular reflection coefficient
+    uniform vec3 k_a;   // Ambient reflection coefficient
+    uniform vec3 k_d;   // Diffuse reflection coefficient
+    uniform vec3 k_s;   // Specular reflection coefficient
     // Material color
     
-    uniform vec3 Ia;
-    uniform vec3 Ip_diffuse_color;
+    uniform vec3 diffuseColor;
     uniform vec3 lightPosition; // Light position
     uniform vec3 lightColor;
-    uniform vec3 objectColor;
     uniform vec3 viewPosition;
 
     // Programa principal do Fragment Shader.
@@ -107,8 +100,7 @@ material.fragmentShader = `
     void main() {
     
       // ambient
-      float ambientStrength = 0.1;
-      vec3 ambient = ambientStrength * lightColor;
+      vec3 ambient = k_a * lightColor;
 
 
       // diffuse
@@ -116,21 +108,20 @@ material.fragmentShader = `
       vec3 L = normalize(lightPosition - fragPosition);
       // N = Normal at point of surface
       // L = normalized vector from point to light source (light direction)
-      vec3 diffuse = max(dot(N, L), 0.0) * lightColor;
+      vec3 diffuse = k_d * max(dot(N, L), 0.0) * diffuseColor;
 
       // specular
       // R = normalized vector of light reflected off the Surface
       // C = normalized vector from point to camera
       // k = constant expoent; larger k makes smaller highlight
-      // specular component = pow(max(dot(R, C), 0), k) * lightColor;
-      float specularStrength = 0.5;
+      // specular component = pow(max(dot(R, C), 0), k) * diffuseColor;
       vec3 R = reflect(-L, normalVector);
       vec3 C = normalize(viewPosition - fragPosition);
-      vec3 specular = specularStrength * pow(max(dot(C, R), 0.0), 16.0) * lightColor;
+      vec3 specular = k_s * pow(max(dot(C, R), 0.0), 16.0) * diffuseColor;
 
 
       // combine
-      gl_FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);
+      gl_FragColor = vec4((ambient + diffuse + specular), 1.0);
     }
     `;
 
