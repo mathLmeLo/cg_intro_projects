60d752feea81145a1735b091bb55c48b591738ec
Matheus Melo <matheus.correia@lavid.ufpb.br>
2022-06-12 22:00:58 -0300

    phong-working-version

--- atividade_4/int_phong.js
+++ atividade_4/int_phong.js
@@ -1,173 +1,156 @@
 function intPhong() {
-        // Criação do objeto Three.js que armazenará os dados da cena.
-    let scene = new THREE.Scene();
+// Criação do objeto Three.js que armazenará os dados da cena.
+let scene = new THREE.Scene();
 
-    // Definição da câmera do Three.js
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 25;
+// Definição da câmera do Three.js
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 25;
 
-    // A câmera é adicionada a cena.
-    scene.add(camera);
+// A câmera é adicionada a cena.
+scene.add(camera);
 
-    // Criação do objeto Three.js responsável por realizar o rendering.
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+// Criação do objeto Three.js responsável por realizar o rendering.
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    // Criação do objeto de controle (interação) da câmera.
-    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+// Criação do objeto de controle (interação) da câmera.
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    //----------------------------------------------------------------------------
-    // 'geometry' : Variável que contém a geometria (informação sobre vértices
-    //     e arestas) do objeto a ser renderizado (um torus, neste caso). É importante 
-    //     observar que, de acordo com o Three.js, a geometria de um objeto não contém
-    //     ainda a informação sobre o seu material.
-    //----------------------------------------------------------------------------
-    let geometry = new THREE.TorusGeometry(10, 3, 16, 25);
+//----------------------------------------------------------------------------
+// 'geometry' : Variável que contém a geometria (informação sobre vértices
+//     e arestas) do objeto a ser renderizado (um torus, neste caso). É importante 
+//     observar que, de acordo com o Three.js, a geometria de um objeto não contém
+//     ainda a informação sobre o seu material.
+//----------------------------------------------------------------------------
+let geometry = new THREE.TorusGeometry(10, 3, 16, 25);
 
-    //----------------------------------------------------------------------------
-    // Variáveis do tipo "uniform", enviadas pela CPU aos shaders :
-    //
-    // * 'Ia' : intensidade (i.e cor) da luz ambiente.
-    // * 'Ip_position' : posição da fonte de luz pontual no Espaço do Universo.
-    // * 'Ip_diffuse_color' : intensidade (i.e cor) do componente difuso da fonte de luz pontual.
-    // * 'k_a' : coeficiente de reflectância ambiente do objeto.
-    // * 'k_d' : coeficiente de reflectância difusa do objeto.
-    // * 'k_s' : coeficiente de reflectância especular do objeto.
-    //----------------------------------------------------------------------------
-    let rendering_uniforms = {
-        Ia: {type: 'vec3', value: new THREE.Color(0.3, 0.3, 0.3)},
-        Ip_position: {type: 'vec3', value: new THREE.Vector3(-20, 10, 10)},
-        Ip_diffuse_color: {type: 'vec3', value: new THREE.Color(0.7, 0.7, 0.7)},
-        k_a: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
-        k_d: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
-        k_s: {type: 'vec3', value: new THREE.Color(1, 1, 1)}
+//----------------------------------------------------------------------------
+// Variáveis do tipo "uniform", enviadas pela CPU aos shaders :
+//
+// * 'Ia' : intensidade (i.e cor) da luz ambiente.
+// * 'lightPosition' : posição da fonte de luz pontual no Espaço do Universo.
+// * 'Ip_diffuse_color' : intensidade (i.e cor) do componente difuso da fonte de luz pontual.
+// * 'k_a' : coeficiente de reflectância ambiente do objeto.
+// * 'k_d' : coeficiente de reflectância difusa do objeto.
+// * 'k_s' : coeficiente de reflectância especular do objeto.
+//----------------------------------------------------------------------------
+let rendering_uniforms = {
+    Ia: {type: 'vec3', value: new THREE.Color(0.3, 0.3, 0.3)},
+    lightPosition: {type: 'vec3', value: new THREE.Vector3(-20, 10, 10)},
+    Ip_diffuse_color: {type: 'vec3', value: new THREE.Color(0.7, 0.7, 0.7)},
+    k_a: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
+    k_d: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
+    k_s: {type: 'vec3', value: new THREE.Color(1, 1, 1)},
+    lightColor: {type: 'vec3', value: new THREE.Color(1, 1, 1)},
+    objectColor: {type: 'vec3', value: new THREE.Color(0.25, 0.25, 0.85)},
+    viewPosition: {type: 'vec3', value: camera.position},
+}
+
+//----------------------------------------------------------------------------
+// Criação do material na forma de um Vertex Shader e um Fragment Shader customizados.
+// Os shaders receberão valores da CPU (i.e. variáveis do tipo 'uniform') por meio da
+// variável 'rendering_uniforms'.
+//----------------------------------------------------------------------------
+let material = new THREE.ShaderMaterial({
+    uniforms: rendering_uniforms,
+    vertexShader:'',
+    fragmentShader: ''
+});
+
+//----------------------------------------------------------------------------
+// Vertex Shader
+//----------------------------------------------------------------------------
+material.vertexShader = `
+    // 'uniform' contendo informação sobre a fonte de luz ambiente.
+
+    varying vec3 fragPosition;
+    varying vec3 normalVector;
+
+    // Programa principal do Vertex Shader.
+
+    void main() {
+    
+        fragPosition = vec3(modelMatrix * vec4(position, 1.0));  // vertex in world space
+        
+        normalVector = normalMatrix * normal; // normal vector in world space
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
     }
+    `;
 
-    //----------------------------------------------------------------------------
-    // Criação do material na forma de um Vertex Shader e um Fragment Shader customizados.
-    // Os shaders receberão valores da CPU (i.e. variáveis do tipo 'uniform') por meio da
-    // variável 'rendering_uniforms'.
-    //----------------------------------------------------------------------------
-    let material = new THREE.ShaderMaterial({
-        uniforms: rendering_uniforms,
-        vertexShader:'',
-        fragmentShader: ''
-    });
+//----------------------------------------------------------------------------
+// Fragment Shader
+//----------------------------------------------------------------------------
+material.fragmentShader = `
+    // 'I' : valor de cor originalmente calculada pelo Vertex Shader, e já interpolada para o fragmento corrente.
+    
+    varying vec4 I;
+  
+    varying vec3 normalVector;  // Surface normal
+    varying vec3 fragPosition;       // Vertex position
+    
+    uniform float k_a;   // Ambient reflection coefficient
+    uniform float k_d;   // Diffuse reflection coefficient
+    uniform float k_s;   // Specular reflection coefficient
+    // Material color
+    
+    uniform vec3 Ia;
+    uniform vec3 Ip_diffuse_color;
+    uniform vec3 lightPosition; // Light position
+    uniform vec3 lightColor;
+    uniform vec3 objectColor;
+    uniform vec3 viewPosition;
 
-    //----------------------------------------------------------------------------
-    // Vertex Shader
-    //----------------------------------------------------------------------------
-    material.vertexShader = `
-        // 'uniform' contendo informação sobre a fonte de luz ambiente.
-        
-        uniform vec3 Ia;
-        
-        // 'uniforms' contendo informações sobre a fonte de luz pontual.
-        
-        uniform vec3 Ip_position;
-        uniform vec3 Ip_diffuse_color;
+    // Programa principal do Fragment Shader.
 
-        // 'uniforms' contendo informações sobre as reflectâncias do objeto.
-        
-        uniform vec3 k_a;
-        uniform vec3 k_d;
-        uniform vec3 k_s;
+    void main() {
+    
+      // ambient
+      float ambientStrength = 0.1;
+      vec3 ambient = ambientStrength * lightColor;
 
-        // 'I' : Variável que armazenará a cor final (i.e. intensidade) do vértice, após a avaliação do modelo local de iluminação.
-        //     A variável 'I' é do tipo 'varying', ou seja, seu valor será calculado pelo Vertex Shader (por vértice)
-        //     e será interpolado durante a rasterização das primitivas, ficando disponível para cada fragmento gerado pela rasterização.
-        
-        varying vec4 I;
 
-        // Programa principal do Vertex Shader.
+      // diffuse
+      vec3 N = normalize(normalVector);
+      vec3 L = normalize(lightPosition - fragPosition);
+      // N = Normal at point of surface
+      // L = normalized vector from point to light source (light direction)
+      vec3 diffuse = max(dot(N, L), 0.0) * lightColor;
 
-        void main() {
-        
-            // 'modelViewMatrix' : variável de sistema que contém a matriz ModelView (4x4).
-            // 'Ip_pos_cam_spc' : variável que armazenará a posição da fonte de luz no Espaço da Câmera.
-            
-            vec4 Ip_pos_cam_spc = modelViewMatrix * vec4(Ip_position, 1.0);
+      // specular
+      // R = normalized vector of light reflected off the Surface
+      // C = normalized vector from point to camera
+      // k = constant expoent; larger k makes smaller highlight
+      // specular component = pow(max(dot(R, C), 0), k) * lightColor;
+      float specularStrength = 0.5;
+      vec3 R = reflect(-L, normalVector);
+      vec3 C = normalize(viewPosition - fragPosition);
+      vec3 specular = specularStrength * pow(max(dot(C, R), 0.0), 16.0) * lightColor;
 
-            // 'position' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
-            // 'P_cam_spc' : variável que contém o vértice (i.e. 'position') transformado para o Espaço de Câmera.
-            //     Observe que 'position' é um vetor 3D que precisou ser levado para o espaço projetivo 4D 
-            //     (i.e., acrescentando-se uma coordenada adicional w = 1.0) para poder ser multiplicado pela
-            //     matriz 'modelViewMatrix' (que é 4x4).
-            
-            vec4 P_cam_spc = modelViewMatrix * vec4(position, 1.0);
 
-            // 'normal' : variável de sistema que contém o vetor normal do vértice (vec3) no espaço do objeto.
-            // 'normalMatrix' : variável de sistema que contém a matriz de normais (3x3) gerada a partir da matriz 'modelViewMatrix'.
-            
-            vec3 N_cam_spc = normalize(normalMatrix * normal);
-
-            // 'normalize()' : função do sistema que retorna o vetor de entrada normalizado (i.e. com comprimento = 1).
-            // 'L_cam_spc' : variável que contém o vetor unitário, no Espaço de Câmera, referente à fonte de luz.
-            
-            vec3 L_cam_spc = normalize(Ip_pos_cam_spc.xyz - P_cam_spc.xyz);
-
-            // 'reflect()' : função do sistema que retorna 'R_cam_spc', isto é, o vetor 'L_cam_spc' refletido 
-            //     em relação o vetor 'N_cam_spc'.
-            
-            vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
-
-            ///////////////////////////////////////////////////////////////////////////////
-            //
-            // Escreva aqui o seu código para implementar os modelos de iluminação com 
-            // Gouraud Shading (interpolação por vértice). 
-            //
-            ///////////////////////////////////////////////////////////////////////////////
-
-            // 'I' : cor final (i.e. intensidade) do vértice.
-            //     Neste caso, a cor retornada é vermelho. Para a realização do exercício, o aluno deverá atribuir a 'I' o valor
-            //     final gerado pelo modelo local de iluminação implementado.
-            
-            // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.
-            
-            gl_Position = projectionMatrix * P_cam_spc;
-        }
-        `;
-
-    //----------------------------------------------------------------------------
-    // Fragment Shader
-    //----------------------------------------------------------------------------
-    material.fragmentShader = `
-        // 'I' : valor de cor originalmente calculada pelo Vertex Shader, e já interpolada para o fragmento corrente.
-        
-        varying vec4 I;
-
-        // Programa principal do Fragment Shader.
-
-        void main() {
-
-            vec3 I_ambiente = Ia*k_a;
-            vec3 I_difuso = Ip_diffuse_color*k_d* max(0.0,dot(N_cam_spc,L_cam_spc));
-            vec3 I_especular = Ip_diffuse_color*k_s * pow(max(0.0, dot(R_cam_spc,normalize(P_cam_spc.xyz))),16.0);
-            I = vec4(I_ambiente + I_difuso + I_especular, 1.0);
-        
-            // 'gl_FragColor' : variável de sistema que conterá a cor final do fragmento calculada pelo Fragment Shader.
-            
-            gl_FragColor = I;
-        }
-        `;
-
-    //----------------------------------------------------------------------------
-    // 'object_mesh' : De acordo com o Three.js, um 'mesh' é a geometria acrescida do material.
-    //----------------------------------------------------------------------------
-    var object_mesh = new THREE.Mesh(geometry, material);
-    scene.add(object_mesh);
-
-    //----------------------------------------------------------------------------
-    // 'render()' : Função que realiza o rendering da cena a cada frame.
-    //----------------------------------------------------------------------------
-    function render() {
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
+      // combine
+      gl_FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);
     }
+    `;
 
-    // Chamada da função de rendering.
-    render();
+//----------------------------------------------------------------------------
+// 'object_mesh' : De acordo com o Three.js, um 'mesh' é a geometria acrescida do material.
+//----------------------------------------------------------------------------
+var object_mesh = new THREE.Mesh(geometry, material);
+scene.add(object_mesh);
+
+//----------------------------------------------------------------------------
+// 'render()' : Função que realiza o rendering da cena a cada frame.
+//----------------------------------------------------------------------------
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+// Chamada da função de rendering.
+render();
+
 }
 
 // exports function globally to browser
